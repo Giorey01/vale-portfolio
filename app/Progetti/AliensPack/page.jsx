695c7633f1eb623ff1398184f38bb8e14a9598ae
Giorey01 <giorey2001@gmail.com>
2024-10-08 16:55:58 +0200

    Adjust alienspack images

--- app/Progetti/AliensPack/page.jsx
+++ app/Progetti/AliensPack/page.jsx
@@ -13,21 +13,42 @@ function AliensPack() {
                     for T-Shirts, Mugs 
                     and Stickers"
         />
-        <div className="grid grid-cols-2 mx-40 gap-1">
-          <Image
-            src="https://d321io5nxf2wuu.cloudfront.net/ValePortfolio/Progetti/AliensPack/alienscup.webp"
-            width={1500}
-            height={1000}
-            className="row-span-2 object-cover hover:border-2 hover:scale-[1.02] hover:z-10 hover:shadow-2xl hover:rounded-lg duration-200"
-          />
-          <ProjectImage
-            src="https://d321io5nxf2wuu.cloudfront.net/ValePortfolio/Progetti/AliensPack/sticker_mockup.webp"
-            alt="sticker"
-          />
-          <ProjectImage
-            src="https://d321io5nxf2wuu.cloudfront.net/ValePortfolio/Progetti/AliensPack/t-shirt_mockup.webp"
-            alt="t-shirt"
-          />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mx-40 mt-10">
+          {/* Tazza */}
+          <div className="flex justify-center">
+            <Image
+              src="https://d321io5nxf2wuu.cloudfront.net/ValePortfolio/Progetti/AliensPack/alienscup.webp"
+              alt="Mug"
+              width={800} // Dimensioni desiderate per la tazza
+              height={300} // Dimensioni desiderate per la tazza
+              className="min-w-80" // Dimensione minima
+            />
+          </div>
+
+          {/* Sottobicchiere e Maglietta */}
+          <div className="flex flex-col gap-4">
+            {/* Sottobicchiere */}
+            <div className="flex justify-center">
+              <Image
+                src="https://d321io5nxf2wuu.cloudfront.net/ValePortfolio/Progetti/AliensPack/sticker_mockup.webp"
+                alt="Coaster"
+                width={800} // Dimensioni desiderate per il sottobicchiere
+                height={300} // Dimensioni desiderate per il sottobicchiere
+                className="min-w-80"
+              />
+            </div>
+
+            {/* Maglietta */}
+            <div className="flex justify-center">
+              <Image
+                src="https://d321io5nxf2wuu.cloudfront.net/ValePortfolio/Progetti/AliensPack/t-shirt_mockup.webp"
+                alt="T-Shirt"
+                width={800} // Dimensioni desiderate per la maglietta
+                height={300} // Dimensioni desiderate per la maglietta
+                className="min-w-80 object-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
