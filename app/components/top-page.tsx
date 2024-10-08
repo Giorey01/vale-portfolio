4b894eefbe17b6f98c7277cd0db1463c14e7d1a6
Giorey01 <giorey2001@gmail.com>
2024-10-08 17:19:38 +0200

    Adjust mobile alienspack

--- app/components/top-page.tsx
+++ app/components/top-page.tsx
@@ -8,13 +8,13 @@ interface TopPageProps {
 
 function TopPage({ titolo, sottotitolo, descrizione }: TopPageProps) {
   return (
-    <div className="mx-40 mt-24 mb-10">
-      <h1 className="text-5xl font-bold max-w-max animate-typing overflow-hidden whitespace-nowrap">
+    <div className="mx-11 md:mx-40 mt-16 md:mt-24 mb-10">
+      <h1 className="text-4xl md:text-5xl font-bold max-w-max animate-typing overflow-hidden whitespace-nowrap">
         {titolo}
       </h1>
       <div className="flex justify-between mt-10">
-        <h2 className="text-4xl">{sottotitolo}</h2>
-        <p className="text-2xl max-w-96 text-right">{descrizione}</p>
+        <h2 className="text-xl md:text-4xl">{sottotitolo}</h2>
+        <p className="text-md md:text-2xl max-w-96 text-right">{descrizione}</p>
       </div>
     </div>
   );
